695468b79c604e3fb77b504862776319142579c1
captain3916 <guoningdong2@163.com>
2019-01-02 11:52:22 +0800

    init3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import './common/css/move-base.css';
+import './common/css/main.css';
 
 class App extends Component {
   render() {
