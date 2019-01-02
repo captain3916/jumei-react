aeedcff38824f7be311a16ea1c296d597091c1ad
captain3916 <guoningdong2@163.com>
2019-01-02 10:45:22 +0800

    init1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from 'App';
+import App from './App';
 
-React.render(
+ReactDOM.render(
   <App></App>,
   document.getElementById('root')
 )
