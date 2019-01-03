55ca764b352973a5699e9d77927fcc6ca7ce6429
向云飞 <figo291@gmail.com>
2019-01-03 18:57:57 +0800

    123

--- src/components/views/Details/index.js
+++ src/components/views/Details/index.js
@@ -2,29 +2,50 @@ import React, { Component } from 'react';
 import './index.scss'
 
 export default class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: {}
+    }
+    // console.log(this.state.list.short_name)
+  }
+
+  componentDidMount(props) {
+    this.setState({
+      list: this.props.location.state
+    })
+
+    console.log(this.props.location.state)
+  }
+
   render() {
+    let name = this.state.list.short_name ? this.state.list.short_name : '';
     return (
       <div id='details'>
         <header className='details_top'>
           <i className='iconfont icon-arrow-left'></i>
-          <div>多媚卡雅清洁冻膜500g</div>
+          <div>{name}</div>
           <i className='iconfont icon-shouye'></i>
         </header>
         <div className='f-img'>
+
+
+
           <img src='http://lorempixel.com/375/375/' alt='' />
         </div>
         <div className='prod_detail'>
           <div className='price_info'>
             <span className='curry_price'>
-              <span>￥279</span>
-              <del>￥335</del>
+              <span>￥{this.state.list.jumei_price}</span>
+              <del>￥{this.state.list.market_price}</del>
             </span>
             <strong>23人已抢购</strong>
           </div>
-          <div class='count_down'>
-            <span>聚美自营</span>
-            纪梵希香榭高定天鹅绒唇膏3.4g，展现你的时尚品味！
-		      </div>
+          <div className='count_down'>
+            <span className='jumeiziying'>聚美自营</span>
+            {this.state.list.name}
+          </div>
         </div>
         <div className='additional-info'>
           <div className='postage-wrap'>
@@ -32,10 +53,21 @@ export default class Details extends Component {
             <div className='postage-content'>
               <span className='tip-word'>该商品最多可享花呗12期分期购买福利</span>
             </div>
+          </div>
+          <div className='postage-wrap'>
+            <div className='postage-type'>运费</div>
+            <div className='postage-content'>
+              <span className='tip-word'>本商品单件包邮</span>
+            </div>
 
           </div>
-          <div></div>
-          <div></div>
+          <div className='postage-wrap'>
+            <div className='postage-type'>说明</div>
+            <div className='postage-content'>
+              <span className='tip-word'>本商品单件包邮</span>
+            </div>
+
+          </div>
 
 
 
