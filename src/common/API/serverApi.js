0b41c2654e518bb4d27693776302342f5e26638a
captain3916 <guoningdong2@163.com>
2019-01-04 12:13:54 +0800

    一起团页面样式OK

--- src/common/API/serverApi.js
+++ src/common/API/serverApi.js
@@ -5,7 +5,7 @@
  * url: 'http://129.204.109.25:3000/yiqituan/tab_list'
  * params: tab: 类型 （可选）
  *         page：请求的是第几页（可选）
- *         per_pag: 每页数据是多少条（可选）
+ *         per_page: 每页数据是多少条（可选）
  * 返回值:{
 *      code: 0 || 1,    // 0代表请求成功
 *      msg： 'OK',  // 请求状态描述
@@ -15,7 +15,7 @@
 *      }
 *  }
  */
-export const Yiqituan = 'http://129.204.109.25:3000/yiqituan/tab_list';
+export const GetYiqituanInfo = 'http://129.204.109.25:3000/yiqituan/tab_list';
 /**
  * 注册接口
  * method:  post
@@ -27,10 +27,10 @@ export const Yiqituan = 'http://129.204.109.25:3000/yiqituan/tab_list';
 *      msg： 'OK',  // 注册状态描述
 *  }
  */
-export const Register = 'http://129.204.109.25:3000/user/register';
+export const RegisterURL = 'http://129.204.109.25:3000/user/register';
 
 /**
- * 注册接口
+ * 登录接口
  * method:  post
  * url: 'http://129.204.109.25:3000/user/register'
  * params: userName: 用户名 （必须传）
@@ -40,7 +40,7 @@ export const Register = 'http://129.204.109.25:3000/user/register';
 *      msg： 'OK',  // 登录状态描述
 *  }
  */
-export const Login = 'http://129.204.109.25:3000/user/login';
+export const LoginURL = 'http://129.204.109.25:3000/user/login';
 
 /**
  * 获取搜索默认关键字接口
@@ -53,7 +53,7 @@ export const Login = 'http://129.204.109.25:3000/user/login';
 *      data: [], // 关键字信息
 *  }
  */
-export const searchKey = 'http://129.204.109.25:3000/search/key';
+export const GetsearchKey = 'http://129.204.109.25:3000/search/key';
 
 /**
  * 首页获取产品信息接口
@@ -69,7 +69,20 @@ export const searchKey = 'http://129.204.109.25:3000/search/key';
 *     data:  数据
 *  }
 */
-export const Product = 'http://129.204.109.25:3000/product/getProduct';
+export const GetAllProduct = 'http://129.204.109.25:3000/product/getProduct';
+
+/**
+ * 首页获取产品信息接口
+ * method:  get
+ * url: 'http://129.204.109.25:3000/product/getOne'
+ * @param item_id: 商品item_id
+ * 返回值:{
+*     code : 0;
+*     msg : 'OK';
+*     data:  商品数据
+*  }
+*/
+export const GetOneProduct = 'http://129.204.109.25:3000/product/getOne';
 
 
 
--- src/components/views/Yiqituan/index.js
+++ src/components/views/Yiqituan/index.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+import { GetYiqituanInfo } from '../../../common/API/serverApi'
 
 import TopBar from '../../component/TopBar';
 import './index.scss';
@@ -16,16 +20,74 @@ class Yiqituan extends Component {
         {title: '鞋类', key: 'coutuan_jewellery'},
         {title: '饰品配饰', key: 'coutuan_shose'},
         {title: '下期预告', key: 'coutuan_home'},
-      ]
+      ],
+      getInfo: {
+        tab: 'coutuan_home',
+        page: 1,
+        per_page: 5,
+        total_Page: 0
+      },
+      allGoods: []
     }
+
+    this.getGoodInfo = this.getGoodInfo.bind(this);
   }
+
+  getGoodInfo() {
+    axios.get(GetYiqituanInfo,{params: this.state.getInfo})
+      .then((response) => {
+        if (response.data.code === 0) {
+          let total_Page = Math.ceil(response.data.data.total / this.state.getInfo.per_page);
+          this.setState((oldState) => ({
+            allGoods: [...(oldState.allGoods),...response.data.data.list],
+            getInfo: Object.assign(oldState.getInfo, {total_Page})
+          }))
+        }
+      })
+  }
+
+  componentDidMount() {
+    this.getGoodInfo();
+  }
+
   render() {
     return (
       <div className='gnd-yiqituan'>
         <div className='gnd-yiqituan-top'>
           <TopBar list={this.state.tabList}></TopBar>
         </div>
-
+        <section className='gnd-yiqituan-product'>
+          <ul>
+            {
+              this.state.allGoods.map((item) => {
+                return (
+                  <li key={item.item_id}>
+                    <Link  to={ '/product/' + item.item_id } className='goods'>
+                      <div className='people-number'>{item.buyer_number_text}</div>
+                      <div className='goods-topsmall'>
+                        <img src={item.image} alt=''/>
+                      </div>
+                      <div className='goods-middle'>
+                        <span className='goods-num'>{item.group_name_tag}</span>
+                        {item.short_name}
+                      </div>
+                      <div className='goods-foot'>
+                        <div className='price-left'>
+                          <span className='ct-price'>{item.jumei_price}</span>
+                          <span className='sc-price'></span>
+                          <span className='jm-price'>{item.single_price}</span>
+                        </div>
+                        <div className='time-right timer'>
+                          <span className='goods-btn'>去开团</span>
+                        </div>
+                      </div>
+                    </Link>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </section>
       </div>
     )
   }
